3ded816c26d5869435a76b0510b1e2976f3c1bd2
Gerhard Preuss <gelipp@gmail.com>
2018-09-25 11:02:04 +0200

    add clock / now display

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
-export default () => (
-  <p>Welcome to Next.js!
-    <style jsx>{`
-      p {
-        color: red;
-      }
-    `}</style>
-  </p>
-)
+export default class Index extends React.Component {
+  state = {}
+
+  componentDidMount () {
+    this.interval = setInterval(() => {
+      this.setState({now: new Date().toLocaleString()})
+    }, 1000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.interval)
+  }
+
+  render () {
+    return (
+      <p>Welcome to Next.js! {this.state.now ? `now: ${this.state.now}` : ''}
+        <style jsx>{`
+          p {
+            color: red;
+          }
+        `}</style>
+      </p>
+    )
+  }
+}
